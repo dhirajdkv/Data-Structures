b786bbfdb6add1676563a4ce7952c74adaedc2fa
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2019-07-12 19:52:21 +0530

    Create binarytreeimplementation.cpp

---
+++ binarytreeimplementation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+class Node
+{
+int data;
+Node* left;
+Node* right;
+};
+
+int AddNode(int data);
+Node* root;
+
+int main() {
+  std::cout << "Hello World!\n";
+}
+
+int AddNode(int data)
+{
+  if(root==null)
+  {
+    root = new Node(data);
+  }
+  else
+  {
+     Node temp = root;
+      while(temp !=NULL)
+      {
+        if(temp->data<data)
+        {
+          temp = temp->left;
+        }
+        else{
+          temp=temp->right;
+        }
+      }
+
+      n1 = new Node(data);
+       if(temp->data<data)
+        {
+          temp->left=n1;
+        }
+        else{
+          temp->right=n1;
+        }
+
+
+  }
+}
