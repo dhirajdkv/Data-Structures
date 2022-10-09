da9f72f0f2209c3b8dc099177d0238856e7d9124
Dhiraj Kumar Varma Dommaraju <50867441+dhirajdkv@users.noreply.github.com>
2022-10-09 19:38:30 +0530

    Sort red, white, blue colours algo

--- DutchNationalFlagAlgo.java
+++ DutchNationalFlagAlgo.java
@@ -27,30 +27,27 @@ class Solution {
   /*
   Dutch National Flag problem solution.
   */
-  public void sortColors(int[] nums) {
-    // for all idx < i : nums[idx < i] = 0
-    // j is an index of element under consideration
-    int p0 = 0, curr = 0;
-    // for all idx > k : nums[idx > k] = 2
-    int p2 = nums.length - 1;
-
-    int tmp;
-    while (curr <= p2) {
-      if (nums[curr] == 0) {
-        // swap p0-th and curr-th elements
-        // i++ and j++
-        tmp = nums[p0];
-        nums[p0++] = nums[curr];
-        nums[curr++] = tmp;
-      }
-      else if (nums[curr] == 2) {
-        // swap k-th and curr-th elements
-        // p2--
-        tmp = nums[curr];
-        nums[curr] = nums[p2];
-        nums[p2--] = tmp;
-      }
-      else curr++;
+  class Solution {
+    public void sortColors(int[] nums) {
+        int lo = 0, mid = 0;
+        int hi = nums.length - 1;
+        int temp;
+        while (mid <= hi) {
+            switch (nums[mid]) {
+                case 0:
+                    temp = nums[lo];
+                    nums[lo++] = nums[mid];
+                    nums[mid++] = temp;
+                    break;
+                case 1:
+                    mid++;
+                    break;
+                case 2:
+                    temp = nums[hi];
+                    nums[hi--] = nums[mid];
+                    nums[mid] = temp;
+                    break;
+            }
+        }
     }
-  }
 }
